9e5a937c40aa89b13f58b4639238acecd60a4c85
Mohd Farhan <mohdfarhan098ali@gmail.com>
2026-09-22 19:13:16 +0530

    Time: 391 ms (34.54%), Space: 304.2 MB (25.45%) - LeetHub

--- 3525-find-x-value-of-array-ii/3525-find-x-value-of-array-ii.cpp
+++ 3525-find-x-value-of-array-ii/3525-find-x-value-of-array-ii.cpp
@@ -2,133 +2,78 @@ class Solution {
 public:
     struct Node {
         int prod;
-        int cnt[5];
-
-        Node() {
-            prod = 1;
-            for (int i = 0; i < 5; i++) {
-                cnt[i] = 0;
-            }
-        }
+        array<int, 5> freq;
+        Node() : prod(1) { freq.fill(0); }
     };
 
-    int n, k;
-    vector<Node> seg;
+    int k;
+    vector<Node> tree;
+    vector<int> nums;
 
-    Node mergeNodes(const Node& left, const Node& right) {
+    Node merge(const Node& L, const Node& R) {
         Node res;
-
-        // Product of the complete segment.
-        res.prod = (left.prod * right.prod) % k;
-
-        // Prefixes completely inside the left segment.
-        for (int r = 0; r < k; r++) {
-            res.cnt[r] += left.cnt[r];
+        res.prod = (1LL * L.prod * R.prod) % k;
+        res.freq = L.freq;
+        for (int r = 0; r < k; ++r) {
+            if (R.freq[r]) {
+                int nr = (1LL * L.prod * r) % k;
+                res.freq[nr] += R.freq[r];
+            }
         }
-
-        // Prefixes consisting of:
-        
-        for (int r = 0; r < k; r++) {
-            int newRemainder = (left.prod * r) % k;
-            res.cnt[newRemainder] += right.cnt[r];
-        }
-
         return res;
     }
 
-    Node makeNode(int value) {
-        Node res;
-
-        value %= k;
-        res.prod = value;
-
-        
-        
-        res.cnt[value] = 1;
-
-        return res;
-    }
-
-    void build(int node, int l, int r, const vector<int>& nums) {
-        if (l == r) {
-            seg[node] = makeNode(nums[l]);
+    void build(int v, int tl, int tr) {
+        if (tl == tr) {
+            tree[v].prod = nums[tl] % k;
+            tree[v].freq[tree[v].prod] = 1;
             return;
         }
-
-        int mid = l + (r - l) / 2;
-
-        build(node * 2, l, mid, nums);
-        build(node * 2 + 1, mid + 1, r, nums);
-
-        seg[node] = mergeNodes(seg[node * 2], seg[node * 2 + 1]);
+        int tm = (tl + tr) / 2;
+        build(v * 2, tl, tm);
+        build(v * 2 + 1, tm + 1, tr);
+        tree[v] = merge(tree[v * 2], tree[v * 2 + 1]);
     }
 
-    void update(int node, int l, int r, int pos, int value) {
-        if (l == r) {
-            seg[node] = makeNode(value);
+    void update(int v, int tl, int tr, int pos, int val) {
+        if (tl == tr) {
+            tree[v].prod = val % k;
+            tree[v].freq.fill(0);
+            tree[v].freq[tree[v].prod] = 1;
             return;
         }
-
-        int mid = l + (r - l) / 2;
-
-        if (pos <= mid) {
-            update(node * 2, l, mid, pos, value);
-        } else {
-            update(node * 2 + 1, mid + 1, r, pos, value);
-        }
-
-        seg[node] = mergeNodes(seg[node * 2], seg[node * 2 + 1]);
+        int tm = (tl + tr) / 2;
+        if (pos <= tm)
+            update(v * 2, tl, tm, pos, val);
+        else
+            update(v * 2 + 1, tm + 1, tr, pos, val);
+        tree[v] = merge(tree[v * 2], tree[v * 2 + 1]);
     }
 
-    Node query(int node, int l, int r, int ql, int qr) {
-        if (ql <= l && r <= qr) {
-            return seg[node];
-        }
-
-        int mid = l + (r - l) / 2;
-
-        if (qr <= mid) {
-            return query(node * 2, l, mid, ql, qr);
-        }
-
-        if (ql > mid) {
-            return query(node * 2 + 1, mid + 1, r, ql, qr);
-        }
-
-        Node leftPart = query(node * 2, l, mid, ql, qr);
-        Node rightPart = query(node * 2 + 1, mid + 1, r, ql, qr);
-
-        return mergeNodes(leftPart, rightPart);
+    Node query(int v, int tl, int tr, int l, int r) {
+        if (l > r)
+            return Node();
+        if (l == tl && r == tr)
+            return tree[v];
+        int tm = (tl + tr) / 2;
+        return merge(query(v * 2, tl, tm, l, min(r, tm)),
+                     query(v * 2 + 1, tm + 1, tr, max(l, tm + 1), r));
     }
 
     vector<int> resultArray(vector<int>& nums, int k,
                             vector<vector<int>>& queries) {
-        this->n = nums.size();
         this->k = k;
-
-        seg.resize(4 * n + 5);
-
-        build(1, 0, n - 1, nums);
-
-        vector<int> result;
-
+        this->nums = nums;
+        int n = nums.size();
+        tree.assign(4 * n, Node());
+        build(1, 0, n - 1);
+        vector<int> ans;
         for (auto& q : queries) {
-            int index = q[0];
-            int value = q[1];
-            int start = q[2];
-            int x = q[3];
-
-             
-            nums[index] = value;
-            update(1, 0, n - 1, index, value);
-
-            // Every possible remaining array is a prefix
-    
+            int idx = q[0], val = q[1], start = q[2], x = q[3];
+            update(1, 0, n - 1, idx, val);
             Node res = query(1, 0, n - 1, start, n - 1);
-
-            result.push_back(res.cnt[x]);
+            ans.push_back(res.freq[x]);
         }
-
-        return result;
+        return ans;
     }
 };
